77a111184123ea289e37c4f26bcbbd7f359afb1b
Gervig <gervig91@gmail.com>
2025-04-07 09:10:13 +0200

    entity setup

--- src/main/java/app/entities/Instructor.java
+++ src/main/java/app/entities/Instructor.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 
+import java.util.Set;
+
 @DynamicUpdate
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,4 +18,12 @@ public class Instructor
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private int phone;
+    private int yearsOfExperience;
+
+    @OneToMany(mappedBy = "instructor", cascade = CascadeType.PERSIST) // when an instructor is persisted so is their lessons
+    private Set<SkiLesson> lessons;
 }
