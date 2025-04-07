f133ab34d7452ed45d30235b0eeeb2b183b00876
Gervig <gervig91@gmail.com>
2025-04-07 09:28:50 +0200

    DTO

---
+++ src/main/java/app/dtos/InstructorDTO.java
@@ -0,0 +1,49 @@
+package app.dtos;
+
+import app.entities.Instructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties
+public class InstructorDTO
+{
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private int phone;
+    private int yearsOfExperience;
+
+    @ToString.Exclude //avoids infinite to.String recursion
+    @JsonIgnore // avoids stackoverflow when creating JSON
+    private Set<SkiLessonDTO> lessons;
+
+    public InstructorDTO(Instructor instructor, boolean includeDetails)
+    {
+        this.id = instructor.getId();
+        this.firstname = instructor.getFirstname();
+        this.lastname = instructor.getLastname();
+        this.email = instructor.getEmail();
+        this.phone = instructor.getPhone();
+        this.yearsOfExperience = instructor.getYearsOfExperience();
+
+        // maps the SkiLessons for an instructor to DTOs if they have any
+        if(includeDetails && !instructor.getLessons().isEmpty())
+        {
+            this.lessons = instructor.getLessons()
+                    .stream()
+                    .map(skiLesson -> new SkiLessonDTO(skiLesson, false))
+                    .collect(Collectors.toSet());
+        }
+    }
+
+}
---
+++ src/main/java/app/dtos/SkiLessonDTO.java
@@ -0,0 +1,48 @@
+package app.dtos;
+
+import app.entities.SkiLesson;
+import app.enums.Level;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties
+public class SkiLessonDTO
+{
+    private Long id;
+    private LocalDateTime starttime;
+    private LocalDateTime endtime;
+    private double longitude;
+    private double latitude;
+    private String name;
+    private BigDecimal price;
+    private Level level;
+
+    private InstructorDTO instructor;
+
+    public SkiLessonDTO(SkiLesson skiLesson, boolean includeDetails)
+    {
+        this.id = skiLesson.getId();
+        this.starttime = skiLesson.getStarttime();
+        this.endtime = skiLesson.getEndtime();
+        this.longitude = skiLesson.getLongitude();
+        this.latitude = skiLesson.getLatitude();
+        this.name = skiLesson.getName();
+        this.price = skiLesson.getPrice();
+        this.level = skiLesson.getLevel();
+
+        if(includeDetails && skiLesson.getInstructor() != null)
+        {
+            this.instructor = new InstructorDTO(skiLesson.getInstructor(), false);
+            this.instructor.getLessons().add(this);
+        }
+
+    }
+}
