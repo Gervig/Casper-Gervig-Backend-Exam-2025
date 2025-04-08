3f09f2d4fdf03d91db4c928e66917d8506698afa
Gervig <gervig91@gmail.com>
2025-04-08 18:30:19 +0200

    Create LocationPopulator.java

---
+++ src/main/java/app/populators/LocationPopulator.java
@@ -0,0 +1,18 @@
+package app.populators;
+
+import app.entities.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationPopulator
+{
+    public static List<Location> populate()
+    {
+        List<Location> locations = new ArrayList<>();
+
+
+
+        return locations;
+    }
+}
